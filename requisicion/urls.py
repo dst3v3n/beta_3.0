a8ca8d893d8986cd22f4276397d1b439396011f0
lauraroseroochoa <lauratatianarosero1@gmail.com>
2024-03-15 09:45:13 -0500

    crud y visualizar

--- requisicion/urls.py
+++ requisicion/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('create/oferta/', create_oferta.as_view() ,  name='create_oferta'),
     path('save/oferta/', save_requi.info_requi ,  name='save_info_requi'),
     path('company/<int:id_myuser>/', consultar_ofer.as_view(), name='consultar_ofer'),
+    path('ver/oferta/', vista.ver_requi ,  name='ver_info_requi'),
+    path('edit/oferta/', save_requi.edit_requi ,  name='edit_info_requi'),
+    path('delete/oferta/', save_requi.delete_requi ,  name='delete_info_requi'),
 ]
